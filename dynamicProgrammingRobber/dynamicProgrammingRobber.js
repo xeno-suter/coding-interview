b8f7cd3c287b53a91112869765717c3ff1472dfe
Xeno Suter <xeno.suter@students.fhnw.ch>
2024-01-06 19:46:54 +0100

    feat: add dynamic programming robber solution

--- dynamicProgrammingRobber/dynamicProgrammingRobber.js
+++ dynamicProgrammingRobber/dynamicProgrammingRobber.js
@@ -3,7 +3,16 @@
  */
 
 function dynamicProgrammingRobber(houses) {
+    let maxMoney = 0;
+    let prevMaxMoney = 0;
 
+    for (let i = 0; i < houses.length; i++) {
+        let currentMaxMoney = Math.max(maxMoney, prevMaxMoney + houses[i]);
+        prevMaxMoney = maxMoney;
+        maxMoney = currentMaxMoney;
+    }
+
+    return maxMoney;
 }
 
 
